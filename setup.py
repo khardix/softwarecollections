3e01b3ea689cf51ce0dfcdadafdd502584e111c3
Jan Staněk <jstanek@redhat.com>
2018-05-31 12:55:42 +0200

    Extract python dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import softwarecollections
-
-# from distutils.core import setup
 from setuptools import setup, find_packages
 
-setup(
+with open("README.md", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+METADATA = dict(
     name="softwarecollections",
     version="0.15",
     description="Software Collection Management Website and Utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jakub Dorňák",
     author_email="jdornak@redhat.com",
     url="https://github.com/sclorg/softwarecollections",
+)
+
+# Basic package requirements for development instance
+REQUIREMENTS = [
+    "django-fas",
+    "django-markdown2",
+    "django-sekizai",
+    "django-simple-captcha",
+    "django-tagging",
+    "django~=1.8.0",
+    "flock",
+    "py3dns",  # pylibravatar missing dependency workaround
+    "pylibravatar",
+    "python3-memcached",
+    "python3-openid",
+    "requests",
+]
+
+# Extra requirements for production instance
+# Note: Prefer RPM installation to `pip install softwarecollections[production]`
+REQUIREMENTS_PRODUCTION = ["mod_wsgi", "psycopg2"]
+
+setup(
+    **METADATA,
+    python_requires=">=3",
+    install_requires=REQUIREMENTS,
+    extras_require=dict(production=REQUIREMENTS_PRODUCTION),
     packages=find_packages(),
-    include_package_data=True,
+    include_package_data=True
 )
